73b42443b057b9c47299f9393402f13a4ef6b480
Matthew Jonat <mjonat@rheglobal.com>
2021-01-25 14:10:02 +0000

    added modal which is toggleable. Cookie information is being retreived from object declared outside of plugin.

--- src/CookieBar/index.js
+++ src/CookieBar/index.js
@@ -1,25 +1,52 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 
 import Button from "../Button";
+import Modal from '../Modal';
 
 import css from './styles.scss';
 import globalStyles from '../../scss/global.scss';
 
-const CookieBar = props => {
-    return (
-        <div className={css.cookiebar_container}>
-            <div className={globalStyles.container}>
-                <div className={globalStyles.col_8}>
-                    <h2>Use of our cookies</h2>
-                    <p>We use necessary cookies to make our site work. We'd also like to set optional analytics cookies to help us improve it. We won't set optional cookies unless you enable them. Using this tool will set a cookie on your device to remember your preferences. <a href="#">Read More</a></p>
+class CookieBar extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            showModal: false
+        }
+    }
+
+    _modal(){
+        if(this.state.showModal){
+            return <Modal primaryColor={this.props.primaryColor} toggleShowModal={() => this._toggleShowModal()} />
+        }
+
+        return null;
+    }
+
+    _toggleShowModal(){
+        this.setState({
+            showModal: !this.state.showModal
+        });
+    }
+
+    render() {
+        return (
+            <div className={css.cookiebar_container}>
+                <div className={globalStyles.container}>
+                    <div className={globalStyles.col_8}>
+                        <h2 style={{color: this.props.primaryColor}}>Use of our cookies</h2>
+                        <p>We use necessary cookies to make our site work. We'd also like to set optional analytics cookies to help us improve it. We won't set optional cookies unless you enable them. Using this tool will set a cookie on your device to remember your preferences. <a href="#">Read More</a></p>
+                    </div>
+                    <div className={globalStyles.col_4}>
+                        <Button primaryColor={this.props.primaryColor} onClick={() => console.log('test')}>Accept All</Button><br />
+                        <Button primaryColor={this.props.primaryColor} inverted onClick={() => this._toggleShowModal()}>Manage Cookies</Button>
+                    </div>
                 </div>
-                <div className={globalStyles.col_4}>
-                    <Button primaryColor={props.primaryColor} onClick={() => console.log('test')}>Accept All</Button><br /><br />
-                    <Button primaryColor={props.primaryColor} inverted onClick={() => console.log('test')}>Manage Cookies</Button>
-                </div>
+                {this._modal()}
             </div>
-        </div>
-    );
+        );
+    }
+
 }
 
 export default CookieBar;
---
+++ src/Modal/index.js
@@ -0,0 +1,34 @@
+import { h } from 'preact';
+
+import Button from '../Button';
+
+import css from './styles.scss';
+
+const Modal = props => {
+    return (
+        <div class={css.modal_container}>
+            <div className={css.modal_section}>
+                <button onClick={props.toggleShowModal}>close</button>
+            </div>
+            <div className={css.modal_section}>
+                <h2 style={{color: props.primaryColor}}>Our use of cookies</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus amet, nunc faucibus in leo, id at donec. Curabitur faucibus maecenas non, tortor. Parturient sapien tempor libero, consectetur. Ultricies odio egestas interdum ut etiam sollicitudin aliquam.</p>
+                <Button primaryColor={props.primaryColor}>Accept All</Button>
+            </div>
+            <div className={css.modal_section}>
+                <h2 style={{color: props.primaryColor}}>Manage Consent Preferences</h2>
+                <ul>
+                    {window.cookieBarSettings.cookies.map(cookie => (
+                        <li>{cookie.name}</li>
+                    ))}
+                </ul>
+            </div>
+            <div className={css.modal_section}>
+                <Button cancel>Reject All</Button>
+                <Button primaryColor={props.primaryColor}>Save and Exit</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
